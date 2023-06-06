f09c2a81e4ee400f0201858a60d8190dec503549
Lawrence Amoafo <rekenburg777@gmail.com>
2023-06-06 08:08:08 +0000

    Add a form to search for books and also implement function to display the search book

--- src/components/displaybooks.js
+++ src/components/displaybooks.js
@@ -1,34 +1,78 @@
-// import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 // import { useParams } from 'react-router-dom';
-// import { fetchBooks } from '../redux/home/homeSlice';
+import { fetchBooks } from '../redux/home/homeSlice';
 
-// function Displaybooks() {
-//   const dispatch = useDispatch();
-//   const { books, status, error } = useSelector((state) => state.books);
-//   const { search } = useParams();
+function Displaybooks() {
+  const dispatch = useDispatch();
+  const { books, status, error } = useSelector((state) => state.books);
+  //   const { search } = useParams();
 
-//   useEffect(() => {
-//     dispatch(fetchBooks(search));
-//   }, [dispatch, search]);
+  //   const [searchBooks, setSearchBooks] = useState('');
+  const [searchResults, setSearchResults] = useState('');
 
-//   if (status === 'loading') {
-//     return (
-//       <div className="flex justify-center items-center h-screen w-screen bg-[#5284DD]">
-//  <div className="h-12 w-12 rounded-full border-white border-4 border-l-gray-500 animate-spin" />
-//       </div>
-//     );
-//   }
+  const handleChange = (e) => {
+    // setSearchBooks(searchValue);
+    // if (searchBooks !== '') {
+    //   const searchData = books.filter((book) => Object.values(book.title)
+    //     .join('')
+    //     .toLowerCase()
+    //     .includes(searchBooks.toLowerCase()));
+    //   setSearchResults(searchData);
+    // } else {
+    //   setSearchResults('No data found');
+    // }
+    const book = e.target.value;
+    setSearchResults(book);
+  };
 
-//   if (error !== null) {
-//     return <h1 style={{ textAlign: 'center' }}>{error}</h1>;
-//   }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(fetchBooks(`${searchResults}`));
+  };
 
-//   return (
-//     <>
-//       b
-//     </>
-//   );
-// }
+  if (status === 'loading') {
+    return (
+      <div className="flex justify-center items-center h-screen w-screen bg-[#5284DD]">
+        <div className="h-12 w-12 rounded-full border-white border-4 border-l-gray-500 animate-spin" />
+      </div>
+    );
+  }
 
-// export default Displaybooks;
+  if (error !== null) {
+    return <h1 style={{ textAlign: 'center' }}>{error}</h1>;
+  }
+
+  return (
+    <>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              placeholder="Search for books"
+              autoComplete="off"
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Search
+          </button>
+        </form>
+
+        {books.map((book) => (
+          <div key={book.id}>
+            {/* <img
+              src={searchResults.imageLinks.thumbnail}
+              alt={searchResults.title}
+            /> */}
+            <h3>{book.volumeInfo.title}</h3>
+            <p>{book.volumeInfo.authors}</p>
+          </div>
+        ))}
+      </section>
+    </>
+  );
+}
+
+export default Displaybooks;
